33fc312600683dba3065d0fb066c93ead6a36003
bjw5035 <send2ugfd@gmail.com>
2022-11-22 00:18:30 +0900

    DB connection test 파일 생성 및 테스트

---
+++ src/test/com.myweb.board/JDBCTests.java
@@ -0,0 +1,32 @@
+package com.myweb.board;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+public class JDBCTests {
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testConnection() {
+
+        try (Connection con =
+                     DriverManager.getConnection(
+                             "jdbc:mysql://localhost:3306/webdb?serverTimezone=UTC",
+                             "user1",
+                             "rootpass1!!")) {
+            System.out.println(con);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+}
